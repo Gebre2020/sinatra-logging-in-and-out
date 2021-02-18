7d28f16d70a56e62f73a10d0971ec08956c6b4cb
Gebremeskel <gebre20122@gmail.com>
2021-02-18 11:50:26 -0600

    commit tree ede77df7169254124d203e012a29335884d40d58

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
   configure do
     set :views, Proc.new { File.join(root, "../views/") }
+    set :public_folder, 'public'
+    set :views, 'app/views'
     enable :sessions unless test?
     set :session_secret, "secret"
   end
@@ -11,14 +13,31 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by(username: params["username"])
+    if @user !=nil && @user.password == params[:password]
+      session["user_id"] = @user.id
+      redirect to '/account'
+    else
+      erb :error
+    end
 
   end
 
   get '/account' do
+    @current_user = User.find_by_id(session[:user_id])
+    if @current_user
+    #if Helpers.is_logged_in?(session)
+      # @user = Helpers.current_user(session)
+      erb :account
+    else
+      erb :error
+    end
 
   end
 
   get '/logout' do
+    session.clear
+    redirect to '/'
 
   end
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
 class Helpers
+
+    # the test for this uses user_id instead of id.  
+    # uses it as a string, but other test uses it as a symbol
+    # binding.pry
+    def self.current_user(session)
+       @user = User.find_by_id(session[:user_id])
+    end
+    
+    def self.is_logged_in?(session)
+        !!session[:user_id]
+    end
+    
 end
